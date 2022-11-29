a5685151f4dba4aaed6c53c42f62cc7481e2776f
Aluno <dinis.bento@my.istec.pt>
2022-11-29 21:36:42 +0000

    aula

---
+++ csharp/Classes1/Classes1/Carro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+    public class Carro :IComparable<Carro>
+    {
+        public String marca;
+        public String modelo;
+        public int velocidade;
+        public int distancia;
+        public Carro(String _marca, string _modelo)
+        {
+            this.marca = _marca;
+            this.modelo = _modelo;
+            this.velocidade = 0;
+            this.distancia = 0;
+
+        }
+        public void ver()
+        {
+            Console.WriteLine("-------------("+ this.modelo +")-----------");
+            Console.WriteLine("Marca:" +  this.marca);
+            Console.WriteLine("Velocidade:{0} Distância:{1}", this.velocidade,this.distancia);
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+
+        public void acelera(int delta) {
+            this.velocidade = delta;
+            this.distancia += delta;
+            this.ver();
+        }
+
+        public int CompareTo(Carro other)
+        {
+            return other.distancia - this.distancia;
+        }
+    }
+}
---
+++ csharp/Classes1/Classes1/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+    public delegate void dlgopera(int a, int b,ref int r);
+    public delegate bool meupredicado(int n);
+    class Utils
+    {
+        public IEnumerable<int>SacaNumeros(int limite, Predicate<int> predicado)
+        {
+            for (int i = 1; i <= limite; i++) {
+                if (predicado(i)) yield return i;
+            }
+        }
+
+        public bool EPrimo(int n)
+        {
+            if (n < 2) return false;
+            int r,quoc,div = 2;
+            do {
+                r = n % div;
+                quoc = n / div++;
+            } while (r!=0 && quoc >=div);
+            return n == 2 || r != 0;
+        }
+       
+
+        public void soma(int x,int y, ref int rslt)
+        {
+            rslt += x + y;
+            Console.WriteLine("Somando... {0}",rslt);
+           
+        }
+        public void subtrai(int x,int y, ref int rslt)
+        {
+            rslt += x - y;
+            Console.WriteLine("Subtraindo... {0}",rslt);
+            
+        }
+    }
+}
